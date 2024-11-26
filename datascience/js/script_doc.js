75cda917bf9d2626678adb967bc91ecdd63b28a6
Lucas Levorato <lucaslevorato@gmail.com>
2024-11-26 09:24:33 -0300

    doc

--- datascience/js/script_doc.js
+++ datascience/js/script_doc.js
@@ -13,3 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebarContainer = document.querySelector(".sidebar-container");
+    const toggleButton = document.querySelector(".toggle-sidebar");
+
+    toggleButton.addEventListener("click", () => {
+        sidebarContainer.classList.toggle("open");
+    });
+
+    const links = document.querySelectorAll(".sidebar a");
+
+    links.forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});

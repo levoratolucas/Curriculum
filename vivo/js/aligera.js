87de4ae98757ac29931cfb3f0fdad567810b781b
levoratolucas <lucaslevorato@gmail.com>
2025-10-22 10:30:36 -0300

    doc

--- vivo/js/aligera.js
+++ vivo/js/aligera.js
@@ -4,31 +4,82 @@ function loadContent(type) {
 
         case "aligera":
             workspace.innerHTML = `
-                <div class="formulario">
-                    <h2>ALIGERA</h2>
-                    <label>Designador:</label>
-                    <input type="text" id="designador" placeholder="Digite aqui">
+            <div class="formulario">
+            <h2>ALIGERA</h2>
+            <label>Designador:</label>
+            <input type="text" id="designador" placeholder="Digite aqui">
                     
                     <label>SINALIZAÇÃO:</label>
                     <select name="" id="r2">
-                        <option value="r2">R2 piloto</option>
-                        <option value="r2ramal">R2 RAMAL</option>
-                        <option value="isdn">ISDN piloto</option>
-                        <option value="isdnramal">ISDN RAMAL</option>
+                    <option value="r2">R2 piloto</option>
+                    <option value="r2ramal">R2 RAMAL</option>
+                    <option value="isdn">ISDN piloto</option>
+                    <option value="isdnramal">ISDN RAMAL</option>
                     </select>
-
+                    
                     <label>CANAIS:</label>
                     <input type="number" id="canais" placeholder="Ex: 11">
 
                     <label>LAN:</label>
                     <input type="text" id="lan" placeholder="172.16.10.20/29">
-
+                    
                     <button onclick="gerarComandoConversor()">Gerar</button>
-                </div>
+                    </div>
                 <div class="terminal">
-                    <p id="output">HUAWEI</p>
+                <p id="output">HUAWEI</p>
                 </div>
-            `;
+                `;
+
+            break;
+
+        case "digistar":
+            workspace.innerHTML = `
+            <div class="formulario">
+                <h1>Adicionando números e redirecionamentos</h1>
+                <div class="input-row" id="ratio">
+                    <div class="ratio">
+                    <input type="radio" id="piloto" checked name="script-type" value="piloto">
+                    <label for="piloto">Piloto</label>
+                    </div>
+                    <div class="ratio">
+                    <input type="radio" id="ramal" name="script-type" value="ramal">
+                    <label for="ramal">Ramal</label>
+                    </div>
+
+                </div>
+                <div class="input-row" id="fust2">
+                    <div class="ip-group">
+                    <input required type="number" id="piloto-form" placeholder="Piloto ex: 4132222222">
+                    </div>
+                    <input type="text" id="lan" placeholder="172.16.10.20/29">
+                </div>
+                <div class="select-container" id="sinaliza">
+                    <p>Sinalização da central:</p>
+                    <select id="quantitySelect">
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10" selected>10</option>
+                    </select>
+                </div>
+                <textarea id="inputText" placeholder="Insira os números no campo abaixo e clique em 'Gerar'.
+
+                    NUMEROS413-125-9996
+                    NUMEROS413-125-9997
+                    NUMEROS413-125-9998
+                    NUMEROS413-125-9999"></textarea>
+                <br>
+                <button onclick="generateDigistar()">Gerar</button>
+                <button id="copyButton" style="display:none;" onclick="copyToClipboard()">Copiar</button>
+                </main>
+                <div class="terminal" id="output"></div>
+                </div>
+            </div>
+  `
 
             break;
 
@@ -60,7 +111,7 @@ function gerarComandoConversor() {
         case "isdnramal":
             output.textContent = gerarScriptAligeraisdn(designador, lan, true, canais)
             break;
-            
+
 
 
 
@@ -72,9 +123,6 @@ function gerarComandoConversor() {
             break;
     }
 }
-
-
-
 function gerarScriptAligeraR2(designador, lan, ramal, canais) {
     const lanParts = separarIP(lan, "LAN");
 
@@ -85,7 +133,6 @@ function gerarScriptAligeraR2(designador, lan, ramal, canais) {
 
     return generateAligeraConfigR2(lanMac, lanRoute, designador, ramal, canais);
 }
-
 function gerarScriptAligeraisdn(designador, lan, ramal, canais) {
     const lanParts = separarIP(lan, "LAN");
 
@@ -96,8 +143,6 @@ function gerarScriptAligeraisdn(designador, lan, ramal, canais) {
 
     return generateAligeraConfig(lanMac, lanRoute, designador, ramal, canais);
 }
-
-
 function generateAligeraConfigR2(lanMac, lanRoute, desig, ramal, canaisCount) {
     ramal = ramal === true ? "yes" : "no";
     const canaisConfig = canais(canaisCount);
@@ -210,7 +255,6 @@ config apply
 
     `;
 }
-
 function generateAligeraConfig(lanMac, lanRoute, desig, ramal, canaisCount) {
     ramal = ramal === true ? "yes" : "no";
     const canaisConfig = canais(canaisCount);
@@ -317,12 +361,9 @@ config save
 config apply
     `;
 }
-
-
-
 function canais(n) {
     n = parseInt(n);
-    if(n > 30){
+    if (n > 30) {
         alert("no maximo 30 canais, foi re ajustado pra 30, caso seja mais que 30, entre em contato com seu gestor")
         n = 30
     }
@@ -350,3 +391,207 @@ function validarIP(ip) {
     const ipRegex = /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\/([0-9]|[12][0-9]|3[0-2])$/;
     return ipRegex.test(ip);
 }
+
+
+function extractNumbersFromLine(line) {
+    const numbers = line.match(/\d+/g);
+    return numbers ? numbers.join('') : '';
+}
+
+function generateAccountCommand(number, accountNumber) {
+    return `account ${accountNumber}\n` +
+        `    user ${number}\n` +
+        `    auth-user ${number}\n` +
+        `    display-name ${number}\n` +
+        `    contact ${number}\n` +
+        `    no restricted-id\n` +
+        `    allow-simult\n` +
+        `    server 1\n  !\n`;
+}
+
+function generateRedirectionCommand(number, position, quantity) {
+    const truncatedNumber = number.toString().slice(-quantity);
+    return `redirection input e1 groups 0 from ${truncatedNumber} to * output voip-operator groups 0 from ${number} to {To}`;
+}
+
+function generate(lanIp1, lanIp2, lanIp3, lanIp4) {
+    const inputText = document.getElementById("inputText").value.trim();
+    const quantity = parseInt(document.getElementById("quantitySelect").value, 10);
+    const lines = inputText.split("\n");
+    const processedNumbers = new Set();
+    const piloto = document.getElementById('piloto-form').value
+    let accounts = generateAccountCommand(piloto, 1);
+    let redirections = '';
+    let accountNumber = 2;
+    let position = 0;
+
+    lines.forEach(line => {
+        const number = extractNumbersFromLine(line);
+        if (number.length >= 9 && !processedNumbers.has(number)) {
+            processedNumbers.add(number);
+            if (number !== piloto) {
+                accounts += generateAccountCommand(number, accountNumber);
+                accountNumber++;
+            }
+            redirections += generateRedirectionCommand(number, position, quantity) + '\n';
+            position++;
+        }
+    });
+
+    let scriptPiloto = `\nredirection input e1 groups 0 from * to * output voip-operator groups 0 from ${piloto} to {To}`;
+    const endred = `redirection input voip-operator groups 0 from * to * output e1 groups 0 from {From:3} to {To:-4}\n\n`;
+    const script = `digistar
+
+        enable
+
+        configure terminal
+
+        debug sip loglevel 5
+
+        debug pbx loglevel 5
+
+        logging on 
+
+        interface ethernet 1
+
+        ip address ${lanIp1}.${lanIp2}.${lanIp3}.${lanIp4 + 2} 255.255.255.248 
+
+        exit
+
+        ip route static inter-vrf 
+
+        ip route 0.0.0.0/0 ${lanIp1}.${lanIp2}.${lanIp3}.${lanIp4 + 1}  
+
+        pbx 
+        digital-line 1 
+        network-mode network 
+        clock-mode master 
+        trunk-mode r2 
+        r2-standard bra 
+        crc-4 automatic 
+        digits 4 
+        enable idc 
+        digit-timeout 4 
+        line-signaling incoming digital outgoing digital 
+        register-signaling incoming mfc outgoing mfc 
+        mfc-absent-time 20 
+        channels 30 
+        occupation increasing 
+
+        exit
+
+
+        digital-line 2 
+        network-mode network 
+        clock-mode master 
+        trunk-mode r2 
+        r2-standard bra 
+        crc-4 automatic 
+        digits 4 
+        enable idc 
+        digit-timeout 4 
+        line-signaling incoming digital outgoing digital 
+        register-signaling incoming mfc outgoing mfc 
+        mfc-absent-time 20 
+        channels 30 
+        occupation increasing
+
+        exit
+
+        exit
+
+        sip 
+        user EX-USER 
+        transport udp port 5060 
+        codec g729 
+        codec fax g711u 
+        no vad  
+        no only_one_codec  
+        dtmf outband 
+        dtmf outband payload 101 
+        cid from uri 
+        no prack    
+        no ignore-hold  
+        no compact-headers  
+        signalling 183-sdp 
+        vsp 1 
+        only-proxy local-ip 
+        no dns-naptr 
+        server 192.168.25.1 5060 
+        no did display-name 
+        no did contact 
+        no did restricted-id 
+        no did from 
+        expire-time 180 
+        no proxy-server  
+        cid from uri 
+        dtmf outband 
+        dtmf outband payload 101 
+        codec g711u 
+        codec fax g711u 
+        no vad  
+        no only_one_codec  
+        transport udp 
+        no prack  
+        no ignore-hold  
+        no compact-headers  
+        timeout 2 
+        signalling 183-sdp 
+        no remote-codec 
+        no shutdown 
+        
+        exit\n`
+    const output = script + accounts + '\nexit\n\npbx\n\n' + endred + redirections + scriptPiloto;
+    document.getElementById("output").innerText = output;
+
+    // Mostrar o botão de copiar
+    document.getElementById("copyButton").style.display = "inline-block";
+}
+
+function generateDigistar() {
+    const lan = document.getElementById("lan").value;
+    const lanParts = separarIP(lan, "LAN");
+    generate(lanParts[0], lanParts[1], lanParts[2], lanParts[3])
+};
+
+function copyToClipboard() {
+    const outputText = document.getElementById("output").innerText;
+    const textarea = document.createElement("textarea");
+    textarea.value = outputText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+
+    // Alerta para o usuário
+    alert("Texto copiado para a área de transferência!");
+}
+document.addEventListener("DOMContentLoaded", function () {
+    // Função para atualizar a visibilidade do campo piloto
+    function updateVisibility() {
+        const scriptType = document.querySelector('input[name="script-type"]:checked').value;
+        const pilotoForm = document.getElementById("piloto-form");
+        const inputText = document.getElementById("inputText");
+        const quantitySelect = document.getElementById("sinaliza");
+
+        if (scriptType === 'ramal') {
+            pilotoForm.style.display = 'block';
+            inputText.style.display = 'block';
+            quantitySelect.style.display = 'block';
+        } else {
+            inputText.style.display = 'none';
+            inputText.style.value = ''
+            quantitySelect.style.display = 'none';
+            pilotoForm.style.display = 'block';
+        }
+    }
+
+    // Adiciona evento change aos botões de rádio
+    const radioButtons = document.querySelectorAll('input[name="script-type"]');
+    radioButtons.forEach(function (radio) {
+        radio.addEventListener('change', updateVisibility);
+    });
+
+    // Chama a função uma vez para definir o estado inicial correto
+    updateVisibility();
+});
